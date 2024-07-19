7df6a7f679b84b7093328c491cdabc187ba434b3
archanabharti2003 <shristibharti718@gmail.com>
2024-07-19 22:17:32 +0530

    eccomerce website(only backend)

--- FINAL PROJECT/ECOMMERCE BACKEND/utils/catchAsync.js
+++
@@ -1,7 +0,0 @@
-const createError = require("http-errors");
-
-module.exports = (fn) => {
-  return (req, res, next) => {
-    fn(req, res, next).catch((err) => next(createError(500, err)));
-  };
-};
